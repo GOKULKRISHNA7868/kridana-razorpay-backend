eda4c123c37cbb2d77a2902de8c2d48bb0722330
Gokul Krishna <145557194+GOKULKRISHNA7868@users.noreply.github.com>
2026-05-03 17:11:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express = require("express");
 const Razorpay = require("razorpay");
 const cors = require("cors");
+const crypto = require("crypto");
 
 const app = express();
 
-// ✅ Allow only your frontend domain
 app.use(
   cors({
-    origin: ["http://localhost:5181", "https://kridana.net"],
+    origin: [
+      "http://localhost:5174",
+      "http://localhost:5181",
+      "https://kridana.net",
+      "capacitor://localhost",
+      "http://localhost",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   }),
 );
+
+app.options("*", cors());
 
 app.use(express.json());
 
